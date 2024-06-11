f401a081d45a4b03ce8daa0e50c8a926629569a9
GuoXuanKun <1925918787@qq.com>
2024-06-11 17:45:19 +0800

    通过 Maven 管理 `邮件发送` 相关依赖

--- src/main/java/day240611/MyEmailUtil.java
+++ src/main/java/day240611/MyEmailUtil.java
@@ -6,17 +6,21 @@ import jakarta.mail.internet.MimeBodyPart;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.mail.internet.MimeMultipart;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
 public class MyEmailUtil {
-    private static final String FROM = "1925918787@qq.com";
-    private static final String SECRET_TOKEN = "hywfeosldxficjgd";
+    public static final Properties PROPERTIES;
+    private static final String FROM;
+    private static final String SECRET_TOKEN;
 
-    public static void send(String name, String email, String content) throws Exception {
-        System.out.println("已经成功把【"+content+"】发给了【" + name + " <" + email+">");
-        // 真正的发
-        // 第三方
-        send(email, content);
+    static {
+        // 读取配置文件，获取整个程序需要的全局配置信息
+        PROPERTIES = loadFromConfiguration();
+        FROM = PROPERTIES.getProperty("from", "admin@xxx.com");
+        SECRET_TOKEN = PROPERTIES.getProperty("secret_token");
     }
 
     public static void send(String email, String content)  throws Exception {
@@ -56,6 +60,23 @@ public class MyEmailUtil {
     }
 
     public static void main(String[] args) throws Exception {
-        MyEmailUtil.send("1925918787@qq.com", "这是一段测试消息");
+        MyEmailUtil.send(PROPERTIES.getProperty("to"), "这是一段测试消息");
+    }
+
+    private static Properties loadFromConfiguration() {
+        Properties properties = new Properties();
+        String fileName = "src/main/java/day240611/config.properties";
+        try {
+            properties.load(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
+            System.out.println("配置文件[" + fileName + "]不存在。");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.out.println("未知的错误！");
+            throw new RuntimeException(e);
+        }
+        //System.out.println(properties);
+        System.out.println("配置文件读取成功");
+        return properties;
     }
 }
