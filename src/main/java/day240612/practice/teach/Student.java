4c0742bb19d47fa4d28bcf5a4c6a347d011a9539
GuoXuanKun <1925918787@qq.com>
2024-06-12 21:29:01 +0800

    (teach) Java Bean、POJO

---
+++ src/main/java/day240612/practice/teach/Student.java
@@ -0,0 +1,42 @@
+package day240612.practice.teach;
+
+/**
+ * Java Bean
+ * POJO
+ */
+public class Student {
+    // state
+    private String name;
+    private int age;
+
+    // behavior
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // constructor
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
