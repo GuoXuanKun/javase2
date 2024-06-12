919d4746a57708c70b3640d0ba4d6ff069c79f14
GuoXuanKun <1925918787@qq.com>
2024-06-12 21:50:03 +0800

    (teach) 引入单元测试 Junit5 - 增加多个测试代码块，可以单独运行也可以批量运行

--- src/test/java/day240612/teach/MyCalculatorTest.java
+++ src/test/java/day240612/teach/MyCalculatorTest.java
@@ -12,4 +12,14 @@ class MyCalculatorTest {
         int actual = MyCalculator.add(1, 2);
         assertEquals(3, actual);
     }
+
+    @Test
+    void testSecretAddWithAssertTrue() {
+        assertTrue(8 == MyCalculator.secretAdd(1, 2));
+    }
+
+    @Test
+    void testSecretAddWithAssertEquals() {
+        assertEquals(8, MyCalculator.secretAdd(1, 2));
+    }
 }
