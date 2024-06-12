6e2873e565b7e262992148752aed41a1a650f10f
GuoXuanKun <1925918787@qq.com>
2024-06-12 21:41:24 +0800

    (teach) Teacher + lombok 注解 ...
    依赖提前导好了

--- src/main/java/day240612/practice/teach/Teacher.java
+++ src/main/java/day240612/practice/teach/Teacher.java
@@ -1,35 +1,15 @@
 package day240612.practice.teach;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
 public class Teacher {
-    private String name;
+    private String namePlus;
     private int age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Teacher(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Teacher{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
