c0368d4f4188805a4f451beb4dcfd1996e78a312
GuoXuanKun <1925918787@qq.com>
2024-06-12 11:58:38 +0800

    Teacher + lombok 注解

--- src/main/java/day240612/practice/me/Teacher.java
+++ src/main/java/day240612/practice/me/Teacher.java
@@ -1,4 +1,15 @@
 package day240612.practice.me;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
 public class Teacher {
+    private String name;
+    private int age;
 }
